aa19eaf6c303f66fb7bbed9a37e22cabdec51bd4
m.habibbolahi <m.habibbolahi@gmail.com>
2025-11-05 16:18:29 +0330

    refactor fcn.py

--- fcn.py
+++ fcn.py
@@ -7,42 +7,27 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from sklearn.model_selection import train_test_split
-
-dir_mask = 'oxford_iiit_pet/mask2'
-dir_pick = 'oxford_iiit_pet/pick2'
-
-INPUT_HEIGHT = 224
-INPUT_WIDTH = 224
+from PIL import Image
 
 
-def load_data(dir_pick, dir_mask):
-    dir_pick_files = os.listdir(dir_pick)
+def load_data(dir_pict, dir_mask):
+    dir_pict_files = os.listdir(dir_pict)
     dir_mask_files = os.listdir(dir_mask)
+    pict = np.empty((len(dir_pict_files), pict_width, pict_height, 3), dtype=np.float16)
+    mask = np.empty((len(dir_mask_files), pict_width, pict_height), dtype=np.float16)
 
-    pick = np.empty((len(dir_pick_files), INPUT_HEIGHT, INPUT_WIDTH, 3), dtype=np.float16)
-    mask = np.empty((len(dir_mask_files), INPUT_HEIGHT, INPUT_WIDTH), dtype=np.float16)
-
-    for i, pick_filename in enumerate(dir_pick_files):
-        pick_filepath = os.path.join(dir_pick, pick_filename)
-        pick[i] = img_to_array(load_img(pick_filepath, target_size=(INPUT_HEIGHT, INPUT_WIDTH)))
+    for i, pict_filename in enumerate(dir_pict_files):
+        pict_filepath = os.path.join(dir_pict, pict_filename)
+        pict[i] = img_to_array(load_img(pict_filepath, target_size=(pict_width, pict_height)))
 
     for i, mask_filename in enumerate(dir_mask_files):
         mask_filepath = os.path.join(dir_mask, mask_filename)
-        mask[i] = img_to_array(load_img(mask_filepath, target_size=(INPUT_HEIGHT, INPUT_WIDTH), color_mode='grayscale')).reshape(INPUT_WIDTH,INPUT_HEIGHT)
+        mask[i] = img_to_array(load_img(mask_filepath, target_size=(pict_width, pict_height), color_mode='grayscale')).reshape(pict_width, pict_height)
 
-    return pick, mask
-
-
-pick, mask = load_data(dir_pick, dir_mask)
-
-x_train, x_val, y_train, y_val = train_test_split(pick, mask, test_size=0.2, random_state=42)
-NUM_CLASSES = len(np.unique(y_train))
-print(NUM_CLASSES)
-y_train = tf.keras.utils.to_categorical(y_train, num_classes=NUM_CLASSES)
-y_val = tf.keras.utils.to_categorical(y_val, num_classes=NUM_CLASSES)
+    return pict, mask
 
 
-def fcn_model(input_shape=(INPUT_HEIGHT, INPUT_WIDTH, 3), num_classes=NUM_CLASSES):
+def fcn_model(input_shape, num_classes):
     inputs = Input(input_shape)
 
     conv1 = Conv2D(32, 3, activation='relu', padding='same')(inputs)
@@ -73,17 +58,35 @@ def fcn_model(input_shape=(INPUT_HEIGHT, INPUT_WIDTH, 3), num_classes=NUM_CLASSE
     model = Model(inputs=inputs, outputs=outputs)
     return model
 
+def main():
+    preprocessed_pict_path = 'oxford_iiit_pet/preprocessed_pict'
+    preprocessed_mask_path = 'oxford_iiit_pet/preprocessed_mask'
+    dir_pict_files = os.listdir(preprocessed_mask_path)
 
-model = fcn_model()
+    pict = Image.open(os.path.join(preprocessed_pict_path, dir_pict_files[0]))
+    pict_width, pict_height = pict.size
 
-model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    pict, mask = load_data(preprocessed_pict_path, preprocessed_mask_path)
+    x_train, x_val, y_train, y_val = train_test_split(pict, mask, test_size=0.2, random_state=42)
+    NUM_CLASSES = len(np.unique(y_train))
+    print(NUM_CLASSES)
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes=NUM_CLASSES)
+    y_val = tf.keras.utils.to_categorical(y_val, num_classes=NUM_CLASSES)
 
-model.summary()
+    model = fcn_model((pict_width, pict_height, 3), NUM_CLASSES)
 
-history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_val, y_val))
+    model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
 
-loss, accuracy = model.evaluate(x_val, y_val)
-print(f'Validation loss: {loss}, Validation accuracy: {accuracy}')
+    model.summary()
 
-model.save('final_model_fcn.h5')
+    history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_val, y_val))
 
+    loss, accuracy = model.evaluate(x_val, y_val)
+    print(f'Validation loss: {loss}, Validation accuracy: {accuracy}')
+
+    model.save('final_model_fcn.h5')
+
+    print('model saved')
+
+if __name__ == '__main__':
+    main()
